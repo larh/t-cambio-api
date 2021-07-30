2c2263cf349b946d72f87a996e8178a65b22b955
Luis Rosales <larh0510@gmail.com>
2021-07-29 23:33:18 -0500

    Eureka

---
+++ srv-eureka/src/test/java/com/bcp/tcambio/eureka/SrvEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcp.tcambio.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
